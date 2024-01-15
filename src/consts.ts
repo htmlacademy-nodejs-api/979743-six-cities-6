7fcddbf2308edb1b4d9c0bbb58f8b2ff11ada243
AnnaGW <a.bogoslovskaja@yandex.ru>
2024-01-15 16:58:53 +0500

    Установила пакеты chalk и dayjs. Написала типы для сущностей по ТЗ. Создала consts.ts

---
+++ src/consts.ts
@@ -0,0 +1,38 @@
+export const USER_TYPES = ['usual', 'pro'] as const;
+
+export const CITIES = [
+  {
+    name: 'Paris',
+    latitude: 48.85661,
+    longitude: 2.351499,
+  },
+  {
+    name: 'Cologne',
+    latitude: 50.938361,
+    longitude: 6.959974,
+  },
+  {
+    name: 'Brussels',
+    latitude: 50.846557,
+    longitude: 4.351697,
+  },
+  {
+    name: 'Amsterdam',
+    latitude: 52.370216,
+    longitude: 4.895168,
+  },
+  {
+    name: 'Hamburg',
+    latitude: 53.550341,
+    longitude: 10.000654,
+  },
+  {
+    name: 'Dusseldorf',
+    latitude: 51.225402,
+    longitude: 6.776314,
+  }
+] as const;
+
+export const HOUSING_TYPES = ['apartment', 'house', 'room', 'hotel'] as const;
+
+export const CONVENIENCES = ['Breakfast', 'Air conditioning', 'Laptop friendly workspace', 'Baby seat', 'Washer', 'Towels', 'Fridge'] as const;
---
+++ src/types/offer-type.ts
@@ -0,0 +1,20 @@
+import { CITIES, HOUSING_TYPES, CONVENIENCES } from '../consts.js';
+
+export type TOffer = {
+  name: string;
+  description: string;
+  date: number; //&&&&
+  city: typeof CITIES;
+  previewImg: string;
+  photos: string[];
+  isPremium: boolean;
+  isFavorites: boolean;
+  rating: number;
+  housingType: typeof HOUSING_TYPES;
+  rooms: number;
+  adults: number;
+  price: number;
+  conveniences: typeof CONVENIENCES;
+  author: string;
+  commentsCount: number;
+}
