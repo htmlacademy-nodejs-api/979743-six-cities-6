ad5c417f287b5132b520f62778e6a7f3cda1b1aa
AnnaGW <a.bogoslovskaja@yandex.ru>
2024-01-19 00:01:54 +0500

    Зарегистрировала команду import. Дописала функцию execute для import. Делала как в демо. Переделала МОКи. Пришлось использовать не значения из enum, а ключи. В консоль выводится нормально.

--- src/cli/command/import.command.ts
+++ src/cli/command/import.command.ts
@@ -1,11 +1,25 @@
 import { Command } from './command.interface.js';
+import { TSVFileReader } from '../../libs/file-reader/tsv-file-reader.js';
 
 export class ImportCommand implements Command {
   public getName(): string {
     return '--import';
   }
 
-  public execute(...parameters: string[]): void {
-    // Чтение файла
+  public execute(...parameters: string[]): void { // на вход должен прийти путь к файлу = строка
+    const [filename] = parameters;
+    const fileReader = new TSVFileReader(filename.trim());
+
+    try {
+      fileReader.read();
+      console.log(fileReader.toArray());
+    } catch(err) {
+      if(!(err instanceof Error)) {
+        throw err; // как-то обрабатываем дальше если это не Error
+      }
+
+      console.error(`Can't import data from file: ${filename}`);
+      console.error(`Details: ${err.message}`);
+    }
   }
 }
