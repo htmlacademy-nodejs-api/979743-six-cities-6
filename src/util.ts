47f559cfd8af1f34fae2ac47f8319d776f263083
AnnaGW <a.bogoslovskaja@yandex.ru>
2024-01-23 22:14:40 +0500

    Установила json-server, прописала сценарий в package.json. Все как в лекции. Все запускается норм. Добавила описание в workflow.md. Сделала заготовку для генерации МОКов. Добавила утильные функции для выбора случайных элементов из массива. Поставила плагин REST-Client для VSCode. Написала запрос в queries.http.

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,20 @@
-// преобразование строки в логический тип
 export function toBoolean(str: string): boolean {
   if (str === 'true') {
     return true;
   }
   return false;
+}
+
+export function generateRandomValue(min:number, max: number, numAfterDigit = 0) {
+  return +((Math.random() * (max - min)) + min).toFixed(numAfterDigit);
+}
+
+export function getRandomItems<T>(items: T[]):T[] {
+  const startPosition = generateRandomValue(0, items.length - 1);
+  const endPosition = startPosition + generateRandomValue(startPosition, items.length);
+  return items.slice(startPosition, endPosition);
+}
+
+export function getRandomItem<T>(items: T[]):T {
+  return items[generateRandomValue(0, items.length - 1)];
 }
