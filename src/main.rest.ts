16027157aafd9ec9f4781b78262e99770f9f15b3
AnnaGW <a.bogoslovskaja@yandex.ru>
2024-02-06 15:58:28 +0500

    Сделала интерфейс для класса конфигурации RestConfig. В RestConfig добавила dotenv в RestConfig. И передачу ему логгера в конструктор. Теперь RestConfig может считать и распарсить переменные из .env файла.

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,10 +1,12 @@
 import { PinoLogger } from './libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
+import { RestConfig } from './libs/config/index.js';
 
 async function bootstrap() {
   const logger = new PinoLogger();
+  const config = new RestConfig(logger);
 
-  const application = new RestApplication(logger);
+  const application = new RestApplication(logger, config);
   await application.init();
 }
 
