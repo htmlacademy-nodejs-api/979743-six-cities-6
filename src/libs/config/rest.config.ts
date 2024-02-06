88a5ba1f2061016e073c51311b05eb8b336562c4
AnnaGW <a.bogoslovskaja@yandex.ru>
2024-02-06 18:35:53 +0500

    Валидация входных данных = настроек. Установила convict и типы. Сделала схему настроек, которая передается в convict. Добавила типизацию для интерфейса Config. Добавила новые переменные окружения.

--- src/libs/config/rest.config.ts
+++ src/libs/config/rest.config.ts
@@ -1,9 +1,11 @@
-import { DotenvParseOutput, config } from 'dotenv';
+import { config } from 'dotenv';
 import { Config } from './config.interface.js';
 import { Logger } from '../logger/index.js';
+import { configRestSchema, TRestSchema } from './rest.schema.js';
 
-export class RestConfig implements Config {
-  private readonly config: NodeJS.ProcessEnv;
+export class RestConfig implements Config<TRestSchema> {
+  private readonly config: TRestSchema;
+
   constructor(
     private readonly logger: Logger
   ) {
@@ -11,11 +13,16 @@ export class RestConfig implements Config {
     if (parsedOutput.error) {
       throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
     }
-    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    // this.config = <DotenvParseOutput>parsedOutput.parsed;
+    configRestSchema.load({});
+    configRestSchema.validate({allowed: 'strict', output: this.logger.info});
+    this.config = configRestSchema.getProperties();
+
     this.logger.info('.env file found and successfully parsed!');
   }
 
-  public get(key: string): string | undefined {
+  // public get(key: string): string | undefined {
+  public get<T extends keyof TRestSchema>(key: T):TRestSchema[T] {
     return this.config[key];
   }
 }
--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,10 +1,10 @@
 import { Logger } from '../libs/logger/index.js';
-import { Config } from '../libs/config/index.js';
+import { Config, TRestSchema } from '../libs/config/index.js';
 
 export class RestApplication {
   constructor(
     private readonly logger: Logger,
-    private readonly config: Config,
+    private readonly config: Config<TRestSchema>,
   ) {}
 
   public async init() {
