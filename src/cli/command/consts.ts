7e7e6efdd61df934f26b3cf89272e188fb56d7a8
AnnaGW <a.bogoslovskaja@yandex.ru>
2024-02-16 19:37:48 +0500

    Доработала команду import. Есть ошибка в saveOffer. И я еще не придумала, как хранить координаты.

--- src/cli/command/consts.ts
+++ src/cli/command/consts.ts
@@ -2,3 +2,6 @@ export const VERSION_COMMAND = '--version';
 export const HELP_COMMAND = '--help';
 export const IMPORT_COMMAND = '--import';
 export const GENERATE_COMMAND = '--generate';
+
+export const DEFAULT_DB_PORT = '27017';
+export const DEFAULT_USER_PASSWORD = '123456';
--- src/cli/command/import.command.ts
+++ src/cli/command/import.command.ts
@@ -1,24 +1,79 @@
 import { Command } from './command.interface.js';
-import { IMPORT_COMMAND } from './consts.js';
+import { DEFAULT_DB_PORT, DEFAULT_USER_PASSWORD, IMPORT_COMMAND } from './consts.js';
 import { TSVFileReader } from '../../libs/file-reader/tsv-file-reader.js';
 import { createOffer } from '../../helpers/offer.js';
 import { getErrorMessage } from '../../helpers/common.js';
+import { DefaultUserService, UserModel, UserService } from '../../libs/models/user/index.js';
+import { DefaultOfferService, OfferModel, OfferService } from '../../libs/models/offer/index.js';
+import { DatabaseClient } from '../../libs/db-client/db-client.interface.js';
+import { Logger } from '../../libs/logger/logger.interface.js';
+import { ConsoleLogger } from '../../libs/logger/console.logger.js';
+import { MongoDatabaseClient } from '../../libs/db-client/mongo.bd-client.js';
+import { IOffer } from '../../types/offer-type.js';
+import { getMongoURI } from '../../helpers/database.js';
 export class ImportCommand implements Command {
+  private userService: UserService;
+  private offerServise: OfferService;
+  private databaseClient: DatabaseClient;
+  private logger: Logger;
+  private salt: string;
+
+  constructor() {
+    this.onImportedLine = this.onImportedLine.bind(this);
+    this.onCompleteImport = this.onCompleteImport.bind(this);
+
+    this.logger = new ConsoleLogger();
+    this.userService = new DefaultUserService(this.logger, UserModel);
+    this.offerServise = new DefaultOfferService(this.logger, OfferModel);
+    this.databaseClient = new MongoDatabaseClient(this.logger);
+  }
+
   public get name(): string {
     return IMPORT_COMMAND;
   }
 
-  private onImportedLine(line: string) {
+  private async saveOffer(offer: IOffer) {
+    const user = await this.userService.findOrCreate({
+      ...offer.user, // в оффере я храню только id автора, как мне передать сюда весь объект автора???
+      password: DEFAULT_USER_PASSWORD,
+    }, this.salt);
+
+    await this.offerServise.create({
+      title: offer.title,
+      description: offer.description,
+      date: offer.date,
+      city: offer.city,
+      previewImg: offer.previewImg,
+      photos: offer.photos,
+      isPremium: offer.isPremium,
+      isFavorites: offer.isFavorites,
+      rating: offer.rating,
+      housingType: offer.housingType,
+      rooms: offer.rooms,
+      adults: offer.adults,
+      price: offer.price,
+      conveniences: offer.conveniences,
+      authorID: user.id,
+      commentsCount: offer.commentsCount,
+    });
+  }
+
+  private async onImportedLine(line: string, resolve: () => void) {
     const offer = createOffer(line);
-    console.info(offer);
+    await this.saveOffer(offer);
+    resolve();
   }
 
   private onCompleteImport(count: number) {
     console.info(`${count} rows imported.`);
+    this.databaseClient.disconnect();
   }
 
-  public async execute(...parameters: string[]): Promise<void> {
-    const [filename] = parameters;
+  public async execute(filename: string, login: string, password: string, host: string, dbname: string, salt: string): Promise<void> {
+    // const [filename] = parameters;
+    const uri = getMongoURI(login, password, host, DEFAULT_DB_PORT, dbname);
+    this.salt = salt;
+    await this.databaseClient.connect(uri);
     const fileReader = new TSVFileReader(filename.trim());
     fileReader.on('line', this.onImportedLine);
     fileReader.on('end', this.onCompleteImport);
